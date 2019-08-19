1532334c29a67c91b682195e0ad042456e4f818e
Viktoriia Vasyltsiv <viktoriia.vasyltsiv.kn.2016@lpnu.ua>
2019-08-19 12:51:47 +0300

    added unit tests for circle and rectangle

---
+++ ClassesTest/CircleTest.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using Classes;
+
+namespace ClassesTest
+{
+    [TestFixture]
+    public class CircleTest
+    {
+        [Test]
+        public void CircumferenceOfCircle_FindingCircumferenceUsingRadius_Calculated()
+        {
+            var sut = new Circle();
+
+            Assert.That(sut.CircumferenceOfCircle(2), Is.EqualTo(12.56636));
+        }
+
+
+        [Test]
+        public void AreaOfCircle_FindingAreaUsingRadius_Calculated()
+        {
+            var sut = new Circle();
+
+            Assert.That(sut.AreaOfCircle(2), Is.EqualTo(12.56636));
+        }
+
+        [Test]
+        public void CircumferenceOfCircle_StaticFindingCircumferenceUsingRadius_Calculated()
+        {
+            Assert.That(StaticCircle.CircumferenceOfCircle(2), Is.EqualTo(12.56636));
+        }
+
+
+        [Test]
+        public void AreaOfCircle_StaticFindingAreaUsingRadius_Calculated()
+        {
+            Assert.That(StaticCircle.AreaOfCircle(2), Is.EqualTo(12.56636));
+        }
+    }
+}
---
+++ ClassesTest/RectangleTest.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using Classes;
+
+namespace ClassesTest
+{
+    [TestFixture]
+    class RectangleTest
+    {
+        [Test]
+        public void Perimeter_SidesOfRectangle_Calculated()
+        {
+            var sut = new Rectangle(new CoordsOfPoint(0, 3), new CoordsOfPoint(7, 0));
+
+            Assert.That(sut.Perimeter(), Is.EqualTo(20));
+        }
+
+
+        [Test]
+        public void Area_SidesOfRectangle_Calculated()
+        {
+            var sut = new Rectangle(new CoordsOfPoint(0, 3), new CoordsOfPoint(7, 0));
+
+            Assert.That(sut.Area(), Is.EqualTo(21));
+        }
+
+        [Test]
+        public void PerimeterProperty_SidesOfRectangle_Calculated()
+        {
+            var sut = new RectangleAutoImplementedProperties(new CoordsOfPoint(0, 3), new CoordsOfPoint(7, 0));
+
+            Assert.That(sut.PerimeterProperty, Is.EqualTo(20));
+        }
+
+
+        [Test]
+        public void AreaProperty_SidesOfRectangle_Calculated()
+        {
+            var sut = new RectangleAutoImplementedProperties(new CoordsOfPoint(0, 3), new CoordsOfPoint(7, 0));
+
+            Assert.That(sut.AreaProperty, Is.EqualTo(21));
+        }
+
+        [Test]
+        public void PerimeterStatic_SidesOfRectangle_Calculated()
+        {
+            Assert.That(StaticRectangle.PerimeterStatic(new CoordsOfPoint(0, 3), new CoordsOfPoint(7, 0)), Is.EqualTo(20));
+        }
+
+
+        [Test]
+        public void AreaStatic_SidesOfRectangle_Calculated()
+        {
+            Assert.That(StaticRectangle.AreaStatic(new CoordsOfPoint(0, 3), new CoordsOfPoint(7, 0)), Is.EqualTo(21));
+        }
+    }
+}
